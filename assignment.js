1ae6bee296a4bddb7a62edad6677099e2f880863
Kalpanabharath <127297361+Kalpanabharath@users.noreply.github.com>
2024-02-28 17:45:28 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,11 @@
+ let nav=document.getElementById("btnid");
+ let dropdown=document.getElementById("takedrop");
+ nav.onclick=function(){
+    
+    // dropdown.style.height="260px"
+    dropdown.classList.toggle("dropdownopen");
+  
+    
+
+ }
+
